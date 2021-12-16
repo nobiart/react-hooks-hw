e89abb7afed7a83b243c260117f760110bc574e1
nobiart <dmitri.besleaga@gmail.com>
2021-12-16 15:14:52 +0200

    Homework 3. React.Children.map()

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import CollapseWrapper from "../common/collapse";
+
+const ListOfElements = ({ children }) => {
+    return React.Children.map(children, (child, i) => {
+        const config = {
+            ...child.props,
+            id: i + 1
+        };
+        return React.cloneElement(child, config);
+    });
+};
+
+ListOfElements.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
 const ChildrenExercise = () => {
     return (
         <CollapseWrapper title="Упражнение">
@@ -10,16 +29,21 @@ const ChildrenExercise = () => {
                 <code>React.Children.map</code> так и{" "}
                 <code>React.Children.toArray</code>
             </p>
-
-            <Component />
-            <Component />
-            <Component />
+            <ListOfElements>
+                <Component />
+                <Component />
+                <Component />
+            </ListOfElements>
         </CollapseWrapper>
     );
 };
 
-const Component = () => {
-    return <div>Компонент списка</div>;
+const Component = ({ id }) => {
+    return <div>{id}. Компонент списка</div>;
+};
+
+Component.propTypes = {
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 };
 
 export default ChildrenExercise;
